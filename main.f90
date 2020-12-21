a1084633aab740defe12c524212de498dcd1b05f
Andris Gulans <andris.gulans@lu.lv>
2020-12-21 10:43:25 +0200

    Work plan

--- main.f90
+++ main.f90
@@ -3,17 +3,78 @@ implicit none
 integer,parameter :: Ngrid = 500
 real(8), allocatable :: r(:)
 real(8), parameter :: Rmax = 10d0
+integer, parameter :: maxscl = 10
+integer :: ir, iscl
 
-integer :: ir
 
 allocate(r(Ngrid))
 
+!read input
+!...
 
 call gengrid(Ngrid,Rmax,r)
 
 do ir=1,Ngrid
   write(*,*) r(ir)
 enddo
+
+
+! Initial guess for potential or WFs(?)
+!...
+
+! ----- version 1 -------
+! Self-consistency loop
+do iscl=1,maxscl
+
+! Construct Hamiltonian 
+
+! Diagonalize
+
+! Normalize WFs
+
+! Construct the Hartree potential
+
+! Construct the exchange-correlation potential
+
+enddo
+
+
+! ----- version 2 -------
+! Self-consistency loop
+!do iscl=1,maxscl
+
+! Diagonalize via the shooting method
+
+! Normalize WFs
+
+! Construct the Hartree potential
+
+! Construct the exchange-correlation potential
+
+!enddo
+
+
+! ----- version 3 -------
+! Self-consistency loop
+!do iscl=1,maxscl
+
+! integrate the screened Poisson equation
+! $\left( \nabla^2+ 2\epsilon \right \psi(\mathbf{r}) = v(\mathbf{r}) \psi(\mathbf{r})$
+
+! Normalize WFs
+
+! Calculate \epsilon as an expectation value
+
+!enddo
+
+
+! ----- version 4 -------
+! Self-consistency loop
+!do iscl=1,maxscl
+
+! Davidson method 
+
+!enddo
 
 deallocate(r)
 

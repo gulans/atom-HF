f0a7b9e35d771e6276115a3fa05d9cf857c58de3
Jānis Užulis <janis.uzulis@lu.lv>
2020-12-23 15:03:06 +0200

    diasim.f90 - changed dsyev to dsbev in eigenvalue/eigenvector solver (gave ~38 % speedup)

--- diasym.f90
+++ diasym.f90
@@ -1,15 +1,24 @@
 subroutine diasym(Ngrid,H,eig)
 implicit none
 integer, intent(in) :: Ngrid
-real(8), intent(in) :: H(Ngrid) 
+real(8), intent(out) :: H(Ngrid,Ngrid)
 real(8), intent(out) :: eig(Ngrid)
-integer :: ir,l,inf
-real(8) :: work(Ngrid**2)
+integer :: i,j,ir,l,inf
+real(8) :: work(Ngrid**2),Aij, AB(2,Ngrid)
+real(8) :: eigvec(Ngrid,Ngrid),work1(3*Ngrid-2)
 
-l=Ngrid**2
 
-call dsyev('V','U',Ngrid,H,Ngrid,eig,work,l,inf)
- !print *,'dsyev info: ', inf
- !print *,'dsyev work(1): ', work(1)
- !print *,'dsyev lwork: ', l
+ AB(2,1)=H(1,1)
+  
+  do j=2,Ngrid
+   do i=1,2
+     AB(i,j)=H(i+j-2,j)
+   enddo
+  enddo
+
+
+ call dsbev('V','U',Ngrid,1,AB,2,eig,eigvec,Ngrid,work1,inf)
+ H=eigvec
+
+
 end subroutine

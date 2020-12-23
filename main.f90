f519933ee02ab2928bb775d70404e4648d3608a9
Jānis Užulis <janis.uzulis@lu.lv>
2020-12-23 11:12:29 +0200

    changes in main.f90 supports higher l values, but closed shells (Ne,Mg,Ar, for example). Shells in input must be sorted primary by l and secondary by n.

--- main.f90
+++ main.f90
@@ -1,20 +1,20 @@
 program atomHF
+
 implicit none
-real(8), PARAMETER :: Pi = 3.1415926535897932384d0
+real(8), PARAMETER :: Pi = 3.1415926535897932384d0, alpha=0.5d0
 !integer,parameter :: Ngrid = 500
-real(8), allocatable :: r(:),vfull(:),vh(:),vxc(:),exc(:),H(:,:),eig(:),psi(:,:),rho(:)
-integer, allocatable :: shell_n(:),shell_l(:)
-real(8), allocatable :: shell_occ(:)
+real(8), allocatable :: r(:),vfull(:),vh(:),vxc(:),exc(:),H(:,:),eig(:),psi(:,:),rho(:),vfull1(:)
+integer, allocatable :: shell_n(:),shell_l(:),count_l(:)
+real(8), allocatable :: shell_occ(:),psi_eig(:)
 
 !real(8), parameter :: Rmax = 10d0
 integer, parameter :: maxscl = 20
-integer :: ir, iscl
+integer :: ir,il,icl,il_icl,iscl,lmax
 
 real(8) :: Z
 real(8) :: Rmax,hh,e1,e2,e3,energy
 integer :: Ngrid, Nshell, ish
-integer :: i
-
+integer :: i,j,countl0
 
 !read input
 read(*,*) 
@@ -32,9 +32,27 @@ enddo
   write(*,*) "shell_occ ", shell_occ(:)
 #endif
 
+lmax=maxval(shell_l)
+allocate(count_l(lmax+1))
 
-allocate(r(Ngrid),vfull(Ngrid),vh(Ngrid),vxc(Ngrid),exc(Ngrid),H(Ngrid,Ngrid),eig(Ngrid),rho(Ngrid))
-allocate(psi(Ngrid,Nshell))
+
+!counts how many particular l shells (there must be a shorter way to do it)
+do i=0,lmax
+ countl0=0
+ do j=1,Nshell
+ if (i.eq.shell_l(j)) then 
+         countl0=countl0+1
+ endif
+ enddo
+ write(*,*)"l=",i," countl=", countl0
+ count_l(i+1)=countl0
+enddo
+ write(*,*) "count_l=",count_l(:)
+
+
+
+allocate(r(Ngrid),vfull(Ngrid),vh(Ngrid),vxc(Ngrid),exc(Ngrid),H(Ngrid,Ngrid),eig(Ngrid),rho(Ngrid),vfull1(Ngrid))
+allocate(psi(Ngrid,Nshell),psi_eig(Nshell))
 
 call gengrid(Ngrid,Rmax,r)
 hh=r(2)-r(1)
@@ -50,46 +68,51 @@ vfull=-Z/r
 ! Self-consistency loop
 
 do iscl=1,maxscl
+#ifdef debug
+  write(*,*) "iteration: ",iscl
+#endif
 
+il_icl=0
+do il=1,lmax+1
   ! Construct Hamiltonian 
-
-  call genham(Ngrid,r,vfull,H)
+  
+  call genham(Ngrid,r,vfull+0.5d0*(il-1)*il*r**(-2d0),H)
 
   ! Diagonalize
   call diasym(Ngrid,H,eig)
 
-#ifdef debug
-  write(*,*) "iteration",iscl, ". Eigenvalues:"
-     do ir=1,Nshell
-       write(*,*) eig(ir)
-     enddo
-#endif
-
   ! Normalize WFs
-  do ish=1,Nshell
-    psi(:,ish)=H(:,ish)  !tas ir Psi*r
-    psi(:,ish)=psi(:,ish)/r
-    call wfnorm(Ngrid,r,psi(:,ish))
+  do icl=1,count_l(il)
+    il_icl=il_icl+1
+    psi(:,il_icl)=H(:,icl)  !tas ir Psi*r
+    psi(:,il_icl)=psi(:,il_icl)/r
+    psi_eig(il_icl)=eig(icl)
+    call wfnorm(Ngrid,r,psi(:,il_icl))
 #ifdef debug
-    Print *,"PSI",ish," nomalised to:", 4*Pi*sum(psi(:,ish)**2*r**2*hh)
+    write(*,*)"PSI l=",il-1," icl=",icl," il_cl=",il_icl," normalised to:",4*Pi*sum(psi(:,il_icl)**2*r**2*hh),&
+            "eig:",psi_eig(il_icl)," occ:",shell_occ(il_icl)
 #endif
   enddo
-  
-  !write 2 wave functions to file 
+enddo
+  !write 3 wave functions to file 
 !  open(11,file='wf.dat',status='replace')
 !  write(11,*)"r psi1 psi2"
 !   do i = 1,Ngrid 
-!     write(11,*)r(i),psi(i,1),psi(i,2)
+!     write(11,*)r(i),psi(i,1)!,psi(i,2)!,psi(i,3)
 !   end do
 !   close(11)
 
 
 ! Calculate density
   rho=0d0*rho
+
+  
+  
   do ish=1,Nshell
-    rho=rho+shell_occ(ish)*psi(:,ish)**2d0
+     rho=rho+shell_occ(ish)*psi(:,ish)**2d0
   enddo
-! Construct the Hartree potential
+
+  ! Construct the Hartree potential
 
   call getvh(Ngrid,r,-4d0*Pi*rho*r,vh)
 
@@ -97,14 +120,17 @@ do iscl=1,maxscl
   call getvxc(Ngrid,rho,vxc,exc)
 
 ! Caulculate energy
-  e1=2d0*eig(1)
+  e1=0
+  do ish=1,Nshell
+    e1=e1+shell_occ(ish)*psi_eig(ish)
+  enddo
   e2=-0.5d0*4d0*Pi*hh*sum(vh*rho*r**2d0)
   e3=4d0*Pi*hh*sum((exc-vxc)*rho*r**2d0)
   energy=e1+e2+e3
   Print *,"(11) E=",e1,"+",e2,"+",e3,"=",energy
  
-  vfull=-Z/r+vh+vxc
-
+  vfull1=-Z/r+vh+vxc
+  vfull=alpha*vfull1+(1-alpha)*vfull
 enddo
 
 
@@ -145,7 +171,7 @@ enddo
 
 !enddo
 
-deallocate(r)
+deallocate(r,vfull,vh,vxc,exc,H,eig,psi,rho,shell_n,shell_l,count_l,shell_occ)
 
 
 end program

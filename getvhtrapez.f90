6194fb74e5865155b611a82d4f9e6d76752fae0d
Jānis Užulis <janis.uzulis@lu.lv>
2020-12-30 20:04:47 +0200

    bugfix in getvhtrapez.f90

--- getvhtrapez.f90
+++ getvhtrapez.f90
@@ -14,14 +14,10 @@ real(8) :: integ1(Ngrid), integ2(Ngrid)
 hh=(r(2)-r(1))/2 !RUPJI
 
 i=1
-f0=4d0*Pi*r(i)**2*rho(i)*hh
-integ1(i)=f0
-
-i=2
 f1=4d0*Pi*r(i)**2*rho(i)*hh
-integ1(i)=integ1(i-1)+f1
+integ1(i)=0
 
-do i=3,Ngrid
+do i=2,Ngrid
   f0=f1
   f1=4d0*Pi*r(i)**2*rho(i)*hh
   integ1(i)=integ1(i-1)+f0+f1
@@ -30,15 +26,11 @@ integ1=integ1/r
 
 
 i=Ngrid
-f0=4d0*Pi*r(i)*rho(i)*hh
-integ2(i)=f0
-
-i=Ngrid-1
 f1=4d0*Pi*r(i)*rho(i)*hh
-integ2(i)=integ2(i+1)+f1
+integ2(i)=0
 
 
-do i=Ngrid-2,1,-1
+do i=Ngrid-1,1,-1
   f0=f1
   f1=4d0*Pi*r(i)*rho(i)*hh
   integ2(i)=integ2(i+1)+f0+f1

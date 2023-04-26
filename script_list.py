25ce52399363699e329aaac15e9882a33b38fc7a
Janis Uzulis <janis.uzulis@lu.lv>
2023-04-26 11:15:28 +0300

    changed nucleus smearing parameter to r_rms in the input generation script and in the gen_vn.f90 subroutine.

--- script_list.py
+++ script_list.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-def get_sigma(el):
+def get_rn(el):
     mass={2:4,10:20,18:40,20:40,30:65,36:84,54:131,86:222}
     try:
         m=mass[el]
@@ -10,38 +10,80 @@ def get_sigma(el):
         print("Nav zināma elementa kodola massa.", el)
     #visscher1997
     rn=(0.836*m**(1/3)+0.570)*1e-15 #in meters
-    rn=(0.863*m**(1/3)+0.571)*1e-15 #in meters
     a0=0.529177249*1e-10 #in meters
     rn=rn/a0 #in a.u.
     ksi=3/(2*rn**2)
-    sigma=1/((2*ksi)**(1/2))
-    return str(sigma)
+    return str(rn)
 
-def get_sigma2(el):
-    sigmas={2:2.07007858208354E-05,
-       10:3.10511442005306E-05,
-       18:3.73988997033959E-05,
-       36:4.61329490496609E-05,
-       54:5.25765779034729E-05,
-       4:2.51999840416E-05,
-       12:3.26395164729E-05,
-       20:3.74326850917E-05,
-       30:4.29653071204E-05,
-       38:4.67304500420E-05,
-       48:5.02386312822E-05,
-       56:5.32764990728E-05,
-       80:5.96114776527E-05,
-       86:6.14472567899E-05}
+def get_rn2(el):
+    rn_list={2:3.5854812559E-05,
+        4:4.3647652710E-05,
+        10:5.3782158977E-05,
+        12:5.6533300866E-05,
+        18:6.4776793917E-05,
+        20:6.4835312443E-05,
+        30:7.4418094895E-05,
+        36:7.9904610996E-05,
+        38:8.0939513733E-05,
+        48:8.7015861884E-05,
+        54:9.1065303450E-05,
+        56:9.2277603243E-05,
+        80:1.0325010801E-04,
+        86:1.0642977075E-04,
+        118:1.1581939044E-04}
+
+      # {2:2.07007858208354E-05,
+      # 10:3.10511442005306E-05,
+      # 18:3.73988997033959E-05,
+      # 36:4.61329490496609E-05,
+      # 54:5.25765779034729E-05,
+      # 4:2.51999840416E-05,
+      # 12:3.26395164729E-05,
+      # 20:3.74326850917E-05,
+      # 30:4.29653071204E-05,
+      # 38:4.67304500420E-05,
+      # 48:5.02386312822E-05,
+      # 56:5.32764990728E-05,
+      # 80:5.96114776527E-05,
+      # 86:6.14472567899E-05}
+    return(rn_list[el])
 
 
+def name_to_number(el_name):
+    num_list={'he':2,
+        'be':4,
+        'ne':10,
+        'mg':12,
+        'ar':18,
+        'ca':20,
+        'zn':30,
+        'kr':36,
+        'sr':38,
+        'cd':48,
+        'xe':54,
+        'ba':56,
+        'hg':80,
+        'rn':86,
+        'og':118}
+    return(num_list[el_name])
 
-    return(sigmas[el])
+def list_to_list(names):
+    numbers=[]
+    for n in names:
+        i=name_to_number(n)
+        numbers.append(i)
+    return (numbers)
 
 
-list=[56,80,86]
+list_of_names=['he','be','ne','mg','ar','ca','zn','kr','sr','cd','xe','ba','hg','rn','og']
+
+
+list_of_names=['he','be','ne','mg','ar','ca','zn','kr','sr','cd','xe','ba','hg','rn','og']
+
+
+list=list_to_list(list_of_names)
 
 nf='run'    #new folder
-tf='all_templates/clasic_config'   #template folder
 tf='all_templates/occ'
 
 generate=True
@@ -59,7 +101,7 @@ if generate:
         Rmin='5d-8'
         Rmax='30'
         Ngrid='2200'
-        sigma=get_sigma2(el)
+        sigma=get_rn2(el)
         #sigma="0d0"
         f1="101"
         f1w="1d0"
